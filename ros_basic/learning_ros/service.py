c449eb808f3baa86645a7fe87ba03c48478d250a
Au Duong Tai <tai.au.cit23@eiu.edu.vn>
2025-07-20 11:02:21 +0700

    modify service.py

--- ros_basic/learning_ros/service.py
+++ ros_basic/learning_ros/service.py
@@ -20,7 +20,7 @@ class SimpleServiceServer(Node):
         self.publisherDrive = self.create_publisher(AckermannDriveStamped, 'drive', 10)
         self.subOdom = self.create_subscription(Odometry, 'odom', self.listenOdom, 10,callback_group=self.group)
         self.subScan = self.create_subscription(LaserScan,'scan',self.listenScan,10,callback_group=self.group)
-        self.j=0.0
+        self.expected_distance = 0.0
         self.timer = None
         self.timer2 = None
         self.initialize = None
@@ -28,24 +28,22 @@ class SimpleServiceServer(Node):
         self.current_x = 0.0
         self.current_y = 0.0
         self.distance = 0.0
-        self.iniDistance=None
-        self.tocDo=0.0
+        self.iniDistance = None
+        self.tocDo = 0.0
        # Khởi tạo trong hàm khởi tạo class
         self.clientStop=False
         
-        # self.j=0.0
     def start_moving(self):
-        msg=AckermannDriveStamped()
-        msg.drive.speed=self.tocDo
-        
-        msg.drive.steering_angle=0.0
+        msg = AckermannDriveStamped()
+        msg.drive.speed = self.tocDo
+        msg.drive.steering_angle = 0.0
         self.publisherDrive.publish(msg)
-    def start_stop(self,test):
+    def start_stop(self, expected_distance):
         print("📡 start_stop called")
-        self.j=test
-        self.iniDistance=None
+        self.expected_distance = expected_distance
+        self.iniDistance = None
         if self.timer2 is None:
-            self.timer2 = self.create_timer(0.001, self.maymet)
+            self.timer2 = self.create_timer(0.001, self.stop_in_expected_distance)
 
     def stop_moving(self):
         if self.timer is not None:
@@ -55,22 +53,12 @@ class SimpleServiceServer(Node):
             stop_msg.drive.speed = 0.0
             self.publisherDrive.publish(stop_msg)
     def stop_temporary(self):
-            msg=AckermannDriveStamped()
-            msg.drive.speed=0.0
-            
-            msg.drive.speed=0.0
-            
+            msg = AckermannDriveStamped()
+            msg.drive.speed = 0.0
+            msg.drive.speed = 0.0
             self.publisherDrive.publish(msg)
 
-    # def timer_callback(self):
-   
-    #         msg = AckermannDriveStamped()
-    #         msg.drive.speed = 1.0
-    #         msg.drive.steering_angle = 0.0
-    #         self.publisherDrive.publish(msg)
-    
     def listenScan(self, scan: LaserScan):
-        
         for i in range(len(scan.ranges)):
             angle_deg = math.degrees(scan.angle_min + i * scan.angle_increment)
             if -11<= angle_deg <= 11:
@@ -81,8 +69,6 @@ class SimpleServiceServer(Node):
                 else:
                     self.start_moving()
 
-
-
     def listenOdom(self, msg:Odometry):
         self.current_x= msg.pose.pose.position.x
         self.current_y = msg.pose.pose.position.y
@@ -90,24 +76,21 @@ class SimpleServiceServer(Node):
             self.initialize = [self.current_x, self.current_y]
         self.currentPose = [self.current_x, self.current_y]
         self.distance = float(sp.sqrt((self.current_x - self.initialize[0])**2 + (self.current_y - self.initialize[1])**2))
-       
-        
-    def maymet(self):        
-        if self.iniDistance ==None:
+
+    def stop_in_expected_distance(self):
+        if self.iniDistance is None:
             self.iniDistance = self.distance
-        distance2=self.distance-self.iniDistance
-        # self.j=float(j)
-        if (distance2>self.j):
+        distance2 = self.distance - self.iniDistance
+        # self.expected_distance=float(expected_distance)
+        if (distance2 > self.expected_distance):
             if self.timer2 is not None:
                 self.timer2.cancel()
                 self.timer2 = None
                 self.timer.cancel()
-                self.timer=None
+                self.timer = None
                 stop_msg = AckermannDriveStamped()
                 stop_msg.drive.speed = 0.0
                 self.publisherDrive.publish(stop_msg)
-                
-        
 
     def serviceCallback(self, req, res):
         if req.ok:  # nếu ok=True thì dừng lại
@@ -121,26 +104,13 @@ class SimpleServiceServer(Node):
             res.msg = "Robot started"
         return res
 
-    
-
-
-
-
-
-
-
-
-
 def main(args=None):
     rclpy.init(args=args)
     try:
         talker = SimpleServiceServer()
-
         # parallel to the ones in DoubleTalker however.
         executor = MultiThreadedExecutor(num_threads=4)
         executor.add_node(talker)
-     
-
         try:
             executor.spin()
         finally:
@@ -149,7 +119,6 @@ def main(args=None):
             talker.destroy_node()
     finally:
         rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
